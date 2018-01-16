b93bd49f7a165b17075fe9e69bbefbf38aa58a62
Allison Senden <32342133+allisonsenden@users.noreply.github.com>
2018-01-16 14:38:39 -0600

    Add files via upload

--- Project2/2fill-in-the-blanks.py
+++ Project2/2fill-in-the-blanks.py
@@ -1,1 +1,16 @@
+quiz_lists = {easy_quiz: """This animal has a big, bushy tail, big ears, and is very quick. It is a ___1___. Whereas,
+a ___2___ is slow and has a hard ___3___. My personal favorite animal is a human's best friend, or
+a ___4___. My sister's favorite animal is a ___5___. They're said to have 9 lives. Sometimes, a ___4___
+and a ___5___ don't get along, so our parents would never let us get both animals at the same time.""", medium_quiz:
+'''___1___ is known as the land of 10,000 lakes. ___2___ is commonly mistaken as the capital
+of the state, but really it's ___3___. ___4___s are abundant in ___1___ as they are the state bird.
+Back in the early days, most businesses started their developement along the ___5___ riverfront.
+Although, it takes a certain type of person to reside here due to the ___6___ weather conditions.''', hard_quiz:
+'''___1___s with their competitve nature have been a key role in people's lives for centuries.
+I've been kicking the ___2___ around, playing soccer since I was a little kid. Soccer has had such an
+impact across the globe as a universal ___1___. There are 2 halves, 11 players per team and a ___2___.
+___3___ is the only country that calls the sport soccer instead of ___4___.''', easy_ans: ['rabbit', 'turtle', 'shell', 'dog', 'cat'],
+medium_ans: ['minnesota', 'minneapolis', 'st. paul', 'st paul', 'loon', 'mississippi', 'cold'], hard_ans:
+['sport', 'ball', 'america', 'futbol'], 'levels': ['easy', 'medium', 'hard']}
 
+print(easy_quiz)
